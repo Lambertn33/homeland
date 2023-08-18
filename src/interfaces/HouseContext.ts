3905ab12a23936d206ceab2714f98f1625dd8002
Lambertn33 <lambertgithub@gmail.com>
2023-08-18 17:15:46 +0200

    feat: remove unused state

--- src/interfaces/HouseContext.ts
+++ src/interfaces/HouseContext.ts
@@ -2,5 +2,4 @@ import { HouseInterface } from "./House";
 
 export interface HouseContextInterface {
   houses: HouseInterface[];
-  isLoading: boolean;
 }
--- src/store/houses-context.tsx
+++ src/store/houses-context.tsx
@@ -1,22 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 import { housesData } from "../data/Houses";
 import { HouseInterface } from "../interfaces/House";
 import { HouseContextInterface } from "../interfaces/HouseContext";
 
 export const HousesContext = React.createContext<HouseContextInterface>({
   houses: [],
-  isLoading: false,
 });
 
 const HousesContextProvider: React.FC<React.PropsWithChildren> = ({
   children,
 }) => {
-  const [houses, setHouses] = useState<HouseInterface[]>(housesData);
-  const [isLoading, setIsLoading] = useState(false);
+  const houses: HouseInterface[] = housesData;
 
   const housesContext = {
     houses: houses,
-    isLoading: isLoading,
   };
   return (
     <HousesContext.Provider value={housesContext}>
